a687c992ed8ea932e769271fffe4cb56d2cdd3bd
merciof <mercio.andrade@ufrpe.br>
2018-11-16 19:51:41 -0300

    adição de suporte ao framework Boostrap 4 e criação de componente 'tela-login'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SettingsComponent } from './components/settings/settings.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { TelaLoginComponent } from './tela-login/tela-login.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { MenuComponent } from './components/menu/menu.component';
     HeaderComponent,
     FooterComponent,
     SettingsComponent,
-    MenuComponent
+    MenuComponent,
+    TelaLoginComponent
   ],
   imports: [
     BrowserModule
